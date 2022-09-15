f64e4672dd4e25e8dc682a3fb170444da0a8eef3
Rafael Cassone <rafaelcassone@gmail.com>
2022-09-15 12:49:02 -0300

    feature-atividade-07

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { SegundoComponent } from './segundo/segundo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponent,
+    SegundoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
